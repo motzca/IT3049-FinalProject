5a31f0d1f53932f02f69b713c77d531be873aac7
motzca <finnickodair11@gmail.com>
2022-07-30 13:46:57 -0400

    Adding background + aliens

--- Scene1.js
+++ Scene1.js
@@ -3,6 +3,13 @@ class Scene1 extends Phaser.Scene{
         super("bootGame");
     }
 
+    preload(){
+        this.load.image("background", "assets/Mini Pixel Pack 3/Space_BG.png");
+        this.load.image("Alan", "assets/Mini Pixel Pack 3/Enemies/Alan (16 x 16).png");
+        this.load.image("BonBon", "assets/Mini Pixel Pack 3/Enemies/Bon_Bon (16 x 16).png");
+        this.load.image("Lips", "assets/Mini Pixel Pack 3/Enemies/Lips (16 x 16).png");
+    }
+
     create(){
         this.add.text(20,20, "Loading game...");
         this.scene.start("playGame");
--- game.js
+++ game.js
@@ -3,14 +3,8 @@ const config = {
     width: 800,
     height: 600,
     backgroundColor: 0x000000,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {y: 0},
-            debug: false
-        }
-    },
-    scene:[Scene1, Scene2]
+    scene:[Scene1, Scene2],
+    pixelArt: true
 }
 
     var game = new Phaser.Game(config);
