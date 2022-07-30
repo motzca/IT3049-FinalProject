9b8902649af5c9a878382535450c88e688de3212
motzca <finnickodair11@gmail.com>
2022-07-30 13:33:08 -0400

    Adding new assets

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
-var config = {
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    backgroundColor: 0x000000,
     physics: {
         default: 'arcade',
         arcade: {
