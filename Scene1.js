38f1c7c4076eacccbf1599186e9ae6c02d17d1a1
motzca <finnickodair11@gmail.com>
2022-07-30 13:55:55 -0400

    Editing asset names

--- Scene1.js
+++ Scene1.js
@@ -4,10 +4,10 @@ class Scene1 extends Phaser.Scene{
     }
 
     preload(){
-        this.load.image("background", "assets/Mini Pixel Pack 3/Space_BG.png");
-        this.load.image("Alan", "assets/Mini Pixel Pack 3/Enemies/Alan (16 x 16).png");
-        this.load.image("BonBon", "assets/Mini Pixel Pack 3/Enemies/Bon_Bon (16 x 16).png");
-        this.load.image("Lips", "assets/Mini Pixel Pack 3/Enemies/Lips (16 x 16).png");
+        this.load.image("background", "assets/MiniPixelPack3/Space_BG.png");
+        this.load.image("Alan", "assets/MiniPixelPack3/Enemies/Alan.png");
+        this.load.image("BonBon", "assets/MiniPixelPack3/Enemies/Bon_Bon.png");
+        this.load.image("Lips", "assets/MiniPixelPack3/Enemies/Lips.png");
     }
 
     create(){
