9373b69965db9c9128433f2285c1c88aa12cd196
motzca <finnickodair11@gmail.com>
2022-07-31 13:19:36 -0400

    Allowing the aliens and background to move

--- Scene2.js
+++ Scene2.js
@@ -17,4 +17,25 @@ class Scene2 extends Phaser.Scene{
 
         this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
     }
+
+    moveAlien(alien, speed){
+        alien.y += speed;
+        if (alien.y > config.height){
+            this.resetAlienPos(alien);
+        }
+    }
+
+    resetAlienPos(alien){
+        alien.y = 0;
+        var randomX = Phaser.Math.Between(0, config.width);
+        ship.x = randomX;
+    }
+
+    update(){
+        this.moveAlien(this.Allen, 1);
+        this.moveAlien(this.BonBon, 2);
+        this.moveAlien(this.Lips, 3);
+
+        this.backgrounf.tilePostionY -= 0.5;
+    }
 }
